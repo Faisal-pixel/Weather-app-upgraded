c097a94d3c42cf0ec1c68c7313e645c620c75c49
Ifeoluwa Oyeyemi <ifeoluwaoyeyemi915@gmail.com>
2024-08-22 11:35:18 +0100

    weather-ui

---
+++ src/Pages/Setting.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Setting.css'
+import { Button } from "@/Components/ui/button";
+
+
+const Setting: React.FC = () => {
+
+    return(
+        <div>
+            <Button/>
+            
+            
+        </div>
+        
+    )
+} 
+
+export default Setting
